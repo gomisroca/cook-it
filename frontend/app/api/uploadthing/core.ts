05070444e1499e0e329ee006e865cd0963bad6a0
Adrià Gomis <gomisroca@gmail.com>
2026-02-15 21:06:38 +0100

    cleanup of comments and logs

--- frontend/app/api/uploadthing/core.ts
+++ frontend/app/api/uploadthing/core.ts
@@ -23,52 +23,29 @@ async function verifyToken() {
   }
 }
 
-// FileRouter for your app, can contain multiple FileRoutes
 export const UploadThingRouter = {
   profilePicture: f({
     image: {
-      /**
-       * For full list of options and defaults, see the File Route API reference
-       * @see https://docs.uploadthing.com/file-routes#route-config
-       */
       maxFileSize: "2MB",
       maxFileCount: 1,
     },
   })
-    // Set permissions and file types for this FileRoute
     .middleware(async () => {
       return await verifyToken();
     })
     .onUploadComplete(async ({ metadata, file }) => {
-      // This code RUNS ON YOUR SERVER after upload
-      console.log("Upload complete for userId:", metadata.userId);
-
-      console.log("file url", file.ufsUrl);
-
-      // !!! Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
       return { uploadedBy: metadata.userId, url: file.ufsUrl };
     }),
   recipeHeaderImage: f({
     image: {
-      /**
-       * For full list of options and defaults, see the File Route API reference
-       * @see https://docs.uploadthing.com/file-routes#route-config
-       */
       maxFileSize: "4MB",
       maxFileCount: 1,
     },
   })
-    // Set permissions and file types for this FileRoute
     .middleware(async () => {
       return await verifyToken();
     })
     .onUploadComplete(async ({ metadata, file }) => {
-      // This code RUNS ON YOUR SERVER after upload
-      console.log("Upload complete for userId:", metadata.userId);
-
-      console.log("file url", file.ufsUrl);
-
-      // !!! Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
       return { uploadedBy: metadata.userId, url: file.ufsUrl };
     }),
   recipeStepImage: f({
