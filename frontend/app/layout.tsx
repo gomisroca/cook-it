eed8a4efb5fa383e2f149d0a63d2a02a31eb04e2
Adrià Gomis <gomisroca@gmail.com>
2026-02-15 15:03:27 +0100

    styling

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -27,10 +27,17 @@ export default function RootLayout({
           routerConfig={extractRouterConfig(UploadThingRouter)}
         />
         <AuthProvider>
-          <div className="min-h-screen bg-white">
+          <div
+            className="
+            min-h-screen 
+            bg-white 
+            bg-[linear-gradient(45deg,#f6f6f6_25%,transparent_25%),linear-gradient(-45deg,#f6f6f6_25%,transparent_25%),linear-gradient(45deg,transparent_75%,#f6f6f6_75%),linear-gradient(-45deg,transparent_75%,#f6f6f6_75%)] 
+            bg-size-[120px_120px] 
+            bg-position-[0_0,0_60px,60px_-60px,-60px_0px]"
+          >
             <header className="flex items-center justify-between py-4 px-5 sm:px-6 lg:px-8">
               <h1
-                className={`${titleFont.className} text-5xl text-red-500 tracking-wide text-shadow-sm text-shadow-black -skew-x-6`}
+                className={`${titleFont.className} bg-white border-4 border-black rounded p-4 -skew-y-4 cursor-pointer active:scale-95 transition-transform text-5xl text-red-500 tracking-wide text-shadow-sm text-shadow-black -skew-x-6`}
               >
                 Cook It!
               </h1>
