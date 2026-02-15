c5e2386005a33bff024060fc5d48b399a57e39b7
Adrià Gomis <gomisroca@gmail.com>
2026-02-15 11:13:52 +0100

    explicit typing

--- backend/src/common/guards/auth.guard.ts
+++ backend/src/common/guards/auth.guard.ts
@@ -1,3 +1,4 @@
+import { JwtUser } from '@/modules/auth/jwt.interface';
 import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { Request } from 'express';
@@ -12,8 +13,8 @@ export class AuthGuard implements CanActivate {
     if (!token) return false;
 
     try {
-      const payload = this.jwtService.verify(token);
-      req.user = payload; // attach to request
+      const payload = this.jwtService.verify<JwtUser>(token);
+      req.user = payload;
       return true;
     } catch {
       return false;
