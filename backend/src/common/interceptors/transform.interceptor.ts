be7e5a08966278b3054e9dde69141bbd88ee149e
Adrià Gomis <gomisroca@gmail.com>
2026-02-16 12:14:14 +0100

    skip transform decorator

--- backend/src/common/interceptors/transform.interceptor.ts
+++ backend/src/common/interceptors/transform.interceptor.ts
@@ -4,7 +4,9 @@ import {
   ExecutionContext,
   CallHandler,
 } from '@nestjs/common';
-import { Observable, map } from 'rxjs';
+import { Reflector } from '@nestjs/core';
+import { map } from 'rxjs';
+import { SKIP_TRANSFORM } from '../decorators/skip-transform.decorator';
 
 interface TransformResponse<T> {
   status: string;
@@ -16,10 +18,14 @@ export class TransformInterceptor<T> implements NestInterceptor<
   T,
   TransformResponse<T>
 > {
-  intercept(
-    _: ExecutionContext,
-    next: CallHandler,
-  ): Observable<TransformResponse<T>> {
+  constructor(private reflector: Reflector) {}
+
+  intercept(context: ExecutionContext, next: CallHandler) {
+    const skip = this.reflector.get<boolean>(
+      SKIP_TRANSFORM,
+      context.getHandler(),
+    );
+    if (skip) return next.handle();
     return next.handle().pipe(map((data: T) => ({ status: 'success', data })));
   }
 }
