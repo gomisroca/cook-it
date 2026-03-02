752ba1193169d857bd594dde50f1d288a14a3f58
Adrià Gomis <gomisroca@gmail.com>
2026-03-02 08:45:40 +0100

    update uploadthing core to use getcurrentuser()

--- frontend/app/api/uploadthing/core.ts
+++ frontend/app/api/uploadthing/core.ts
@@ -1,62 +1,30 @@
-import { JwtPayload } from "@/contexts/AuthContext";
-import { jwtDecode } from "jwt-decode";
+import { getCurrentUser } from "@/lib/auth";
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 import { UploadThingError } from "uploadthing/server";
 
 const f = createUploadthing();
 
-async function verifyToken(authHeader: string) {
-  const token = authHeader.split(" ")[1];
-  if (!token) throw new UploadThingError("Unauthorized: No token");
-
-  try {
-    const decoded = jwtDecode<JwtPayload>(token);
-
-    // Check if token is expired
-    if (decoded.exp * 1000 < Date.now()) {
-      throw new UploadThingError("Unauthorized: Token expired");
-    }
-
-    return { userId: decoded.id };
-  } catch {
-    throw new UploadThingError("Unauthorized: Invalid token");
-  }
+async function verifyRequest() {
+  const user = await getCurrentUser();
+  if (!user) throw new UploadThingError("Unauthorized");
+  return { userId: user.id };
 }
 
 export const UploadThingRouter = {
-  profilePicture: f({
-    image: {
-      maxFileSize: "2MB",
-      maxFileCount: 1,
-    },
-  })
-    .middleware(async ({ req }) => {
-      return await verifyToken(req.headers.get("Authorization")!);
-    })
+  profilePicture: f({ image: { maxFileSize: "2MB", maxFileCount: 1 } })
+    .middleware(verifyRequest)
     .onUploadComplete(async ({ metadata, file }) => {
       return { uploadedBy: metadata.userId, url: file.ufsUrl };
     }),
-  recipeHeaderImage: f({
-    image: {
-      maxFileSize: "4MB",
-      maxFileCount: 1,
-    },
-  })
-    .middleware(async ({ req }) => {
-      return await verifyToken(req.headers.get("Authorization")!);
-    })
+
+  recipeHeaderImage: f({ image: { maxFileSize: "4MB", maxFileCount: 1 } })
+    .middleware(verifyRequest)
     .onUploadComplete(async ({ metadata, file }) => {
       return { uploadedBy: metadata.userId, url: file.ufsUrl };
     }),
-  recipeStepImage: f({
-    image: {
-      maxFileSize: "1MB",
-      maxFileCount: 1,
-    },
-  })
-    .middleware(async ({ req }) => {
-      return await verifyToken(req.headers.get("Authorization")!);
-    })
+
+  recipeStepImage: f({ image: { maxFileSize: "1MB", maxFileCount: 1 } })
+    .middleware(verifyRequest)
     .onUploadComplete(async ({ metadata, file }) => {
       return { uploadedBy: metadata.userId, url: file.ufsUrl };
     }),
