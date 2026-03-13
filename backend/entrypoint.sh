94f9b899463e8865d15d080e5b2ae941c7e1e85f
Adrià Gomis <gomisroca@gmail.com>
2026-03-13 08:55:31 +0100

    fix prisma schema location find

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "Running Prisma migrations..."
-npx prisma migrate deploy
+npx prisma migrate deploy --schema ./prisma/schema.prisma
 
 echo "Starting server..."
 exec node dist/main.js
